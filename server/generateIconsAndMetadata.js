86ff2f41739fe18332f10c63ae0887540052debd
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2015-12-01 19:21:22 -0800

    instead of generating icons as a script, do it as a promise

---
+++ server/generateIconsAndMetadata.js
@@ -0,0 +1,81 @@
+import fs from 'fs.extra'
+import path from 'path'
+import favicons from 'favicons'
+import Promise from 'bluebird'
+
+
+function saveIcons(directory, images) {
+  const savedFiles = []
+  for (const img of images) {
+    const absFilename = path.join(directory, img.name)
+    savedFiles.push(fs.writeFile(absFilename, img.contents, 'binary'))
+  }
+  return Promise.all(savedFiles)
+}
+
+function saveFiles(directory, files) {
+  const savedFiles = []
+  for (const file of files) {
+    const absFilename = path.join(directory, file.name)
+    savedFiles.push(fs.writeFile(absFilename, file.contents, 'utf-8'))
+  }
+  return Promise.all(savedFiles)
+}
+
+function saveHtmlIconsMetadata(directory, html) {
+  return new Promise((resolve, reject) => {
+    const absFilename = path.join(directory, 'icons-metadata.json')
+    // It's okay to swallow the unlink error -- we don't care if the file doesn't exist.
+    fs.unlink(absFilename, (/* err */) => fs.writeFileSync(absFilename, JSON.stringify(html), 'utf-8'))
+    resolve()
+  })
+}
+
+export default function generateIconsAndMetadata(baseUrl) {
+  return new Promise((resolve, reject) => {
+    const publicDir = path.join(__dirname, '..', 'public')
+    const publicAssetsDir = path.join(publicDir, 'assets')
+    const distDir = path.join(__dirname, '..', 'dist')
+    const distAssetsDir = path.join(distDir, 'assets')
+    const sourceImage = path.join(publicAssetsDir, 'learners-guild-icon.png')
+    const config = {
+      appName: 'icons',                           // Your application's name. `string`
+      appDescription: 'Icons Service',            // Your application's description. `string`
+      developerName: 'Learners Guild',            // Your (or your developer's) name. `string`
+      developerURL: 'https://learnersguild.org',  // Your (or your developer's) URL. `string`
+      background: '#fff',             // Background colour for flattened icons. `string`
+      path: `${baseUrl}/assets`,      // Path for overriding default icons path. `string`
+      url: `${baseUrl}/assets`,       // Absolute URL for OpenGraph image. `string`
+      display: 'standalone',          // Android display: 'browser' or 'standalone'. `string`
+      orientation: 'portrait',        // Android orientation: "portrait" or "landscape". `string`
+      version: null,                  // Your application's version number. `number`
+      logging: false,                 // Print logs to console? `boolean`
+      online: false,                  // Use RealFaviconGenerator to create favicons? `boolean`
+      icons: {
+        android: true,                // Create Android homescreen icon. `boolean`
+        appleIcon: true,              // Create Apple touch icons. `boolean`
+        appleStartup: true,           // Create Apple startup images. `boolean`
+        coast: true,                  // Create Opera Coast icon. `boolean`
+        favicons: true,               // Create regular favicons. `boolean`
+        firefox: true,                // Create Firefox OS icons. `boolean`
+        opengraph: true,              // Create Facebook OpenGraph image. `boolean`
+        twitter: true,                // Create Twitter Summary Card image. `boolean`
+        windows: true,                // Create Windows 8 tile icons. `boolean`
+        yandex: true                  // Create Yandex browser icon. `boolean`
+      }
+    }
+
+    favicons(sourceImage, config, (error, response)=> {
+      if (error) {
+        reject(error)
+      } else {
+        fs.mkdirpSync(distAssetsDir)
+        Promise.all([
+          saveIcons(distAssetsDir, response.images),
+          saveFiles(distAssetsDir, response.files),
+          saveHtmlIconsMetadata(distAssetsDir, response.html),
+        ]).then(resolve)
+      }
+    })
+  })
+}
