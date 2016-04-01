903690ca784ee9a4ed75dfe921e6c8742acc7968
jeffreywescott <jeffrey@learnersguild.org>
2016-04-01 11:19:48 -0700

    add another controller to return list of logo files

---
+++ server/controllers/Logos.js
@@ -0,0 +1,12 @@
+import fs from 'fs'
+import path from 'path'
+
+export function getAvailableLogos(req, res) {
+  const serverPort = parseInt(process.env.PORT, 10)
+  const baseURL = process.env.APP_BASEURL || `http://localhost:${serverPort}`
+  const assetsPath = path.join(__dirname, '..', '..', 'public', 'assets')
+  const logoFiles = fs.readdirSync(assetsPath).filter(filename => filename.match(/^learners-guild-logo/))
+  const logoURLs = logoFiles.map(file => `${baseURL}/assets/${file}`)
+
+  res.status(200).json(logoURLs)
+}
