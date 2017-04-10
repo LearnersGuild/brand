d19d189d17967a3a6e0b367e2d7f4d25617387e1
jeffreywescott <jeffrey@learnersguild.org>
2017-04-10 16:08:38 -0700

    add echo icons

--- server/controllers/Logos.js
+++ server/controllers/Logos.js
@@ -4,7 +4,7 @@ import path from 'path'
 export function getAvailableLogos(req, res) {
   const baseURL = process.env.APP_BASEURL
   const assetsPath = path.join(__dirname, '..', '..', 'public', 'assets')
-  const logoFiles = fs.readdirSync(assetsPath).filter(filename => filename.match(/^learners-guild-logo/))
+  const logoFiles = fs.readdirSync(assetsPath).filter(filename => filename.match(/(png|svg|jpg)/i))
   const logoURLs = logoFiles.map(file => `${baseURL}/assets/${file}`)
 
   res.status(200).json(logoURLs)
